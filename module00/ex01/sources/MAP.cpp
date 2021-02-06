9f268ce294cc265729656442fb6f33b0997b06fb
Sami DARNAUD <samidarnaud@gmail.com>
2021-02-06 17:15:19 +0100

    try to correct 00/01

--- module00/ex01/sources/MAP.cpp
+++ module00/ex01/sources/MAP.cpp
@@ -6,7 +6,7 @@
 /*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 14:34:07 by sadarnau          #+#    #+#             */
-/*   Updated: 2021/02/03 13:40:25 by sadarnau         ###   ########.fr       */
+/*   Updated: 2021/02/06 17:14:16 by sadarnau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,67 +17,27 @@
 #include <limits>
 #include "Contact.class.hpp"
 
-// void print_contacts(Contact contact[], int i)
-
-// void	Contact::print_contacts(Contact contact[], int i)
-// {
-// 	/*	index | first_name | last_name | nickname */
-	
-// 	int	j(-1);
-
-// 	std::cout << std::endl << std::setfill (' ') << std::setw (10);
-// 	std::cout << "index" << '|';
-// 	std::cout << std::setw (10);
-// 	std::cout << "first name" << '|';
-// 	std::cout << std::setw (10);
-// 	std::cout << "last name" << '|';
-// 	std::cout << std::setw (10);
-// 	std::cout << "nickname" << '|' << std::endl;
-// 	std::cout<< std::setfill('-')<< std::setw(44)<<"-"<< std::endl;
-// 	while (++j <= i)
-// 	{
-// 		std::string f_name, l_name, n_name;
-		
-// 		if(contact[j].first_name.size() > 10)
-// 			f_name = contact[j].first_name.substr(0, 9) + '.';
-// 		else
-// 			f_name = contact[j].first_name;
-// 		if(contact[j].last_name.size() > 10)
-// 			l_name = contact[j].last_name.substr(0, 9) + '.';
-// 		else
-// 			l_name = contact[j].last_name;
-// 		if(contact[j].nickname.size() > 10)
-// 			n_name = contact[j].nickname.substr(0, 9) + '.';
-// 		else
-// 			n_name = contact[j].nickname;
-
-// 		std::cout << std::setfill (' ') << std::setw (10);
-// 		std::cout << j << '|';
-// 		std::cout << std::setw (10);
-// 		std::cout << f_name << std::setprecision(10) << '|';
-// 		std::cout << std::setw (10);
-// 		std::cout << l_name << '|';
-// 		std::cout << std::setw (10);
-// 		std::cout << n_name << '|' << std::endl;
-// 	}
-// }
-
 int	choose_contact(int i)
 {
-	int bad_input;
-	int	k;
+	int 		bad_input;
+	int			k;
+	std::string	str;
 
+	k = 0;
 	do {
 		std::cout << std::endl << "Choose an index number for more informations : ";
 		bad_input = 0;
-		std::cin >> k;
-		if(!std::cin)
+		getline(std::cin, str);
+		// std::cin >> k;
+		// k = std::stoi(str);
+
+		if(!std::cin || str.length() > 1)
 		{
 			bad_input = 1;
 			std::cin.clear();
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+			std::cout << "No contact at this index... plz try again" << std::endl;		
 		}
-
 	} while(bad_input || (k < 0 || k > i));
 	
 	return k;
@@ -98,7 +58,8 @@ int	main()
 	while (BCL)
 	{
 		std::cout << std::endl << "What is your command chief ? ";
-		std::cin >> cmd;
+		// std::cin >> cmd;
+		getline(std::cin, cmd);
 
 		if (cmd.compare("ADD") == 0)
 		{

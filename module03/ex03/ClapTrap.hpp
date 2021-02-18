4e30c0afe35ea84ba7095da222072aec3efbaaaf
Sami DARNAUD <samidarnaud@gmail.com>
2021-02-18 16:19:54 +0100

    03/03

---
+++ module03/ex03/ClapTrap.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/18 14:39:30 by sadarnau          #+#    #+#             */
+/*   Updated: 2021/02/18 15:52:32 by sadarnau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_H
+#define CLAPTRAP_H
+
+# include <string>
+# include <iostream>
+# include <ctime>
+
+class ClapTrap  
+{
+protected:
+
+	int			hitPoints;
+	int			maxHitPoints;
+	int			energyPoints;
+	int			maxEnergyPoints;
+	int			level;
+	std::string	name;
+	int			meleeAttackDamage;
+	int			rangedAttackDamage;
+	int			armorDamageReduction;
+
+public:
+
+	ClapTrap(void);									//default constructor
+	ClapTrap( std::string name );					//constructor
+	ClapTrap( ClapTrap const & src);  				//copy
+	~ClapTrap( void );								//destructor
+	ClapTrap & operator=( ClapTrap const & rhs);	//overload operators
+
+	void		takeDamage( unsigned int amount );
+	void		beRepaired( unsigned int amount );
+	int			getHitPoints( void ) const;
+	int			getEnergyPoints( void ) const;
+	int			getLevel( void ) const;
+	int			getMelee( void ) const;
+	int			getRanged( void ) const;
+	int			getArmor( void ) const;
+	std::string	getName( void ) const;
+};
+
+#endif
---
+++ module03/ex03/main.cpp
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/18 11:15:51 by sadarnau          #+#    #+#             */
+/*   Updated: 2021/02/18 16:19:23 by sadarnau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+#include "NinjaTrap.hpp"
+#include "ClapTrap.hpp"
+
+int	main( void )
+{
+	srand (time(NULL));
+
+	/////////////// FRAGTRAP ///////////////////
+
+	FragTrap	*first, *second;
+	
+	first = new FragTrap("Jack");
+	second = new FragTrap();
+
+	std::cout << std::endl;
+	
+	first->meleeAttack("Frank");
+	first->rangedAttack("Francis");
+
+	std::cout << std::endl;
+
+	first->takeDamage(60);
+	first->takeDamage(30);
+	first->takeDamage(100);
+	std::cout << "hitpoints : " << first->getHitPoints() << std::endl;
+
+	std::cout << std::endl;
+
+	first->beRepaired(250);
+	std::cout << "hitpoints : " << first->getHitPoints() << std::endl;
+
+	std::cout << std::endl;
+
+	first->vaulthunter_dot_exe("Georges");
+	first->vaulthunter_dot_exe("Georges");
+	first->vaulthunter_dot_exe("Georges");
+	first->vaulthunter_dot_exe("Georges");
+	first->vaulthunter_dot_exe("Georges");
+	first->vaulthunter_dot_exe("Georges");
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	/////////////// SCAVTRAP ///////////////////
+
+	ScavTrap	*Scav1, *Scav2;
+
+	Scav1 = new ScavTrap("The Door Handler");
+	Scav2 = new ScavTrap();
+	
+	std::cout << std::endl;
+	
+	Scav1->meleeAttack("Frank");
+	Scav2->rangedAttack("Francis");
+
+	std::cout << std::endl;
+	
+	Scav1->takeDamage(60);
+	Scav1->takeDamage(30);
+	Scav1->takeDamage(100);
+	std::cout << "hitpoints : " << Scav1->getHitPoints() << std::endl;
+
+	std::cout << std::endl;
+
+	Scav1->beRepaired(250);
+	std::cout << "hitpoints : " << Scav1->getHitPoints() << std::endl;
+
+	std::cout << std::endl;
+
+	Scav1->challengeNewcomer();
+	std::cout << std::endl;
+	Scav1->challengeNewcomer();
+	std::cout << std::endl;
+	Scav1->challengeNewcomer();
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	/////////////// NINJATRAP ///////////////////
+
+	NinjaTrap	*Ninja1, *Ninja2;
+
+	Ninja1 = new NinjaTrap("SpacePoop");
+	Ninja2 = new NinjaTrap();
+
+	std::cout << std::endl;
+	
+	Ninja1->meleeAttack("Frank");
+	Ninja2->rangedAttack("Francis");
+
+	std::cout << std::endl;
+	
+	Ninja1->takeDamage(60);
+	Ninja1->takeDamage(30);
+	Ninja1->takeDamage(100);
+	std::cout << "hitpoints : " << Ninja1->getHitPoints() << std::endl;
+
+	std::cout << std::endl;
+
+	Ninja1->beRepaired(250);
+	std::cout << "hitpoints : " << Ninja1->getHitPoints() << std::endl;
+
+	std::cout << std::endl;
+
+	Ninja1->ninjaShoebox( *first );
+	std::cout << std::endl;
+	Ninja1->ninjaShoebox( *Scav1 );
+	std::cout << std::endl;
+	Ninja1->ninjaShoebox( *Ninja2 );
+	std::cout << std::endl;
+	ClapTrap *Clap1;
+	Clap1 = new ClapTrap("ClapTest");
+	Ninja1->ninjaShoebox( *Clap1 );
+
+	std::cout << std::endl << std::endl;
+
+	std::cout << "Deleting everything :" << std::endl << std::endl << std::endl;
+
+	delete(first);
+	delete(second);
+	std::cout << std::endl;
+	delete(Scav1);
+	delete(Scav2);
+	std::cout << std::endl;
+	delete(Clap1);
+	std::cout << std::endl;
+	delete(Ninja1);
+	delete(Ninja2);
+	return (0);
+}

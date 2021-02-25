5afceceb37d01d1339e3006aa82c1039ef5858b6
samphoto <samidarnaud@gmail.com>
2021-02-25 18:49:28 +0100

    correcting segfault on 04/01 (overload operator <<)

--- module04/ex01/Character.cpp
+++ module04/ex01/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 22:31:09 by sadarnau          #+#    #+#             */
-/*   Updated: 2021/02/24 21:02:19 by sadarnau         ###   ########.fr       */
+/*   Updated: 2021/02/25 18:48:10 by sadarnau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 Character::Character( std::string const & name ) : name(name), AP(40)
 {
+	this->weapon = NULL;
 	return ;
 }
 

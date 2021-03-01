d4ecb5f9953ba047c41d5fafed39d16542c1d326
Sami DARNAUD <samidarnaud@gmail.com>
2021-03-01 15:29:19 +0100

    05 leaks...

--- module05/ex03/Intern.cpp
+++ module05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 14:00:38 by sadarnau          #+#    #+#             */
-/*   Updated: 2021/03/01 15:08:12 by sadarnau         ###   ########.fr       */
+/*   Updated: 2021/03/01 15:28:25 by sadarnau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,10 @@ Form *	Intern::makeForm( std::string const &  form, std::string const &  target 
 
 	if (!returnForm)
 		std::cout << "Intern didn't find any form with this name... (" << form << ")" << std::endl;
+	else
+		for (int i = 0; i < 3; i++)
+			if (allForms[i] != returnForm)
+				delete allForms[i];
 
 	return returnForm;
 }

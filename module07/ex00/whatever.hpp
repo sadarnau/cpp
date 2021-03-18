4c3037e161e7120f6ac8862b16d41683d6e6dde0
samphoto <samidarnaud@gmail.com>
2021-03-18 16:13:09 +0100

    07/00 clean hpp

--- module07/ex00/whatever.hpp
+++ module07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 15:54:07 by sadarnau          #+#    #+#             */
-/*   Updated: 2021/03/18 16:03:14 by sadarnau         ###   ########.fr       */
+/*   Updated: 2021/03/18 16:12:53 by sadarnau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,5 @@ T const &	max(T const & var1, T const & var2)
 {
 	return (var2 >= var1 ? var2 : var1 );
 }
-
-
-class
-  Awesome {
-    public:
-      Awesome( int n ) : _n( n ) {}
-      bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
-      bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
-      bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
-      bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
-      bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
-      bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
-  private:
-      int _n;
-  };
-
 
 #endif

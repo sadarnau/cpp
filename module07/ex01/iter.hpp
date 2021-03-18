8809aeee7438acdf9f00213be4cf688f93ef59aa
samphoto <samidarnaud@gmail.com>
2021-03-18 16:37:04 +0100

    07/01

---
+++ module07/ex01/iter.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/18 16:15:24 by sadarnau          #+#    #+#             */
+/*   Updated: 2021/03/18 16:36:47 by sadarnau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <string>
+# include <iostream>
+
+template < typename T >
+void	iter(T * array, int len, void (*f)(T const &))
+{
+    for (int i = 0; i < len; i++)
+		f(array[i]);
+}
+
+template < typename T >
+void	print(T const & var)
+{
+	std::cout << var << std::endl;
+}
+
+#endif
---
+++ module07/ex01/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/18 16:15:20 by sadarnau          #+#    #+#             */
+/*   Updated: 2021/03/18 16:36:27 by sadarnau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Iter.hpp"
+
+int main(void)
+{
+	std::cout << "\nTesting on Int :\n\n";
+
+	int arInt[5] = { 0, 1, 2, 3, 4 };
+	iter(arInt, 5, &print);
+	std::cout << std::endl;
+
+	std::cout << "\nTesting on Double :\n\n";
+	double arDouble[5] = { 0.03, 1.1, 2.0, 3.4, 4.009 };
+	iter(arDouble, 5, &print);
+	std::cout << std::endl;
+
+	std::cout << "\nTesting on string :\n\n";
+	std::string arString[] = { "My", "name", "is", "Sam"};
+	iter(arString, 4, &print);
+
+	std::cout << "\nTesting on Bool :\n\n";
+	bool arBool[4] = { 0, 1, 1, 0 };
+	iter(arBool, 4, &print);
+	std::cout << std::endl;
+
+	return (0);
+}

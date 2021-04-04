15e29aba6349038794ac3e7c8c09f0af25453650
Sami DARNAUD <samidarnaud@gmail.com>
2021-04-04 14:20:18 +0200

    08/00 2

--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 11:31:07 by sadarnau          #+#    #+#             */
-/*   Updated: 2021/03/29 12:49:00 by sadarnau         ###   ########.fr       */
+/*   Updated: 2021/04/04 14:11:40 by sadarnau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,41 @@ int	main( void )
 	std::vector<int> vec;
 	std::vector<int>::iterator it;
 	
-	std::cout << "Empty vec test" << std::endl;
+	std::cout << "\n-------- Testing with empty vec --------" << std::endl;
+	std::cout << "         (searching '3' in vec)\n" << std::endl;
 
 	if (easyfind(vec, 3) == vec.end())
 		std::cout << "not found\n";
 	else
 		std::cout << "found !\n";
 
+	std::cout << "\n-------- Testing with simple vec (0 to 4) --------" << std::endl;
+	std::cout << "          (searching '2' in vec)\n" << std::endl;
+
+	vec.push_back(0);
 	vec.push_back(1);
 	vec.push_back(2);
 	vec.push_back(3);
 	vec.push_back(4);
 
-	if (easyfind(vec, 3) == vec.end())
+	if (easyfind(vec, 2) == vec.end())
 		std::cout << "not found\n";
 	else
 		std::cout << "found !\n";
+
+	std::cout << "          (searching '0' in vec)\n" << std::endl;
+
+	if (easyfind(vec, 0) == vec.end())
+		std::cout << "not found\n";
+	else
+		std::cout << "found !\n";
+
+	std::cout << "          (searching '5' in vec)\n" << std::endl;
+
+	if (easyfind(vec, 5) == vec.end())
+		std::cout << "not found\n";
+	else
+		std::cout << "found !\n";
+
 	return (0);
 }
